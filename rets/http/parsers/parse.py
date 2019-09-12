f02e2ed3756faa8a92911abc4c96bbd39268e1d7
kenny <kenny@hackcapital.com>
2019-09-12 10:22:07 -0700

    [parser] Adjust parser to decode only unicode strings (str type) and not bytes.

--- rets/http/parsers/parse.py
+++ rets/http/parsers/parse.py
@@ -15,7 +15,12 @@ ResponseLike = Union[Response, BodyPart]
 
 
 def parse_xml(response: ResponseLike) -> etree.Element:
-    root = etree.fromstring(response.content.decode(DEFAULT_ENCODING), parser=etree.XMLParser(recover=True))
+    content = response.content
+
+    if isinstance(content, str):
+        content = content.decode(DEFAULT_ENCODING)
+
+    root = etree.fromstring(content, parser=etree.XMLParser(recover=True))
 
     if root is None:
         raise RetsResponseError(response.content, response.headers)
